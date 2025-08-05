f102af925210d87053a3cfe507a62d963ec647b0
Josh <lluckyjoshi659@gmail.com>
2025-08-05 19:53:16 +0530

    Nextt

--- express/index.js
+++ express/index.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const port = 3000;
 
+//<%- include(includes/header.ejs) %>
+
 app.set("view engine", "ejs");
 // app.get("/", (req , res) => {
 //     res.render("home.ejs");
